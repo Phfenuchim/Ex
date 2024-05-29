b9485e305e1e5e6df95d08ba2a8fa0a3903b4bf2
Paulo H <phfenuchim@outlook.com>
2024-05-28 21:52:34 -0300

    main

--- src/view/Jogo.java
+++ src/view/Jogo.java
@@ -13,35 +13,35 @@ public class Jogo {
     }
 
     public void iniciar() {
-        // Coloque bombas aleatoriamente no tabuleiro
+
         Random random = new Random();
-        int numeroDeBombas = (int) (tabuleiro.getTamanho() * tabuleiro.getTamanho() * 0.2); // 20% das células terão bombas
+        int numeroDeBombas = (int) (tabuleiro.getTamanho() * tabuleiro.getTamanho() * 0.2); 
         for (int i = 0; i < numeroDeBombas; i++) {
             int linha = random.nextInt(tabuleiro.getTamanho());
             int coluna = random.nextInt(tabuleiro.getTamanho());
             tabuleiro.getCelula(linha, coluna).colocarBomba();
         }
 
-        // Jogo principal
+        
         while (true) {
-            // Exiba o tabuleiro visual
+           
             exibirTabuleiro();
 
-            // Peça ao usuário para escolher uma posição
+            
             System.out.print("Digite a linha (0 a " + (tabuleiro.getTamanho() - 1) + "): ");
             int linha = scanner.nextInt();
             System.out.print("Digite a coluna (0 a " + (tabuleiro.getTamanho() - 1) + "): ");
             int coluna = scanner.nextInt();
 
-            // Verifique se a célula escolhida tem uma bomba
+            
             if (tabuleiro.getCelula(linha, coluna).temBomba()) {
                 System.out.println("BOOM! Você encontrou uma bomba!");
                 break;
             } else {
-                // Contar bombas ao redor e atualizar o valor da célula
+                
                 int bombasAoRedor = contarBombasAoRedor(linha, coluna);
                 tabuleiro.getCelula(linha, coluna).setValor((char) ('0' + bombasAoRedor));
-                tabuleiro.getCelula(linha, coluna).revelar(); // Revela a célula após ser selecionada
+                tabuleiro.getCelula(linha, coluna).revelar(); 
             }
         }
     }
